3681ff34158b0ceb225aade9632ee52fbad686af
A-phenomenal1 <niteshch200053@gmail.com>
2024-06-18 18:49:56 +0530

    first commit of movie app

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+import styles from "./app.module.scss";
+
+function App() {
+  return (
+    <main className={styles.main}>
+      <h1>This is a Movie Application</h1>
+    </main>
+  );
+}
+
+export default App;
